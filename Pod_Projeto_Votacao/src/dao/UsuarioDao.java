9800e45d089660146bdc3ea000d69f5d63af135c
Fatinha de Sousa <fatinha.sg@hotmail.com>
2014-10-22 16:39:02 -0200

    Atualização das classes

--- Pod_Projeto_Votacao/src/dao/UsuarioDao.java
+++ Pod_Projeto_Votacao/src/dao/UsuarioDao.java
@@ -25,13 +25,12 @@ public class UsuarioDao implements UsuarioDaoIT {
     
     @Override
     public void persisteUsuario(Usuario usuario) {
-       String sql = "insert into Usuario(email, token, status) values(?, ?, ?)";
+       String sql = "insert into Usuario(token, status) values(?, ?)";
        
         try {
             PreparedStatement stat = this.conn.prepareCall(sql);
-            stat.setString(1, usuario.getEmail());
-            stat.setString(2, usuario.getToken());
-            stat.setBoolean(3, usuario.getStatus());
+            stat.setString(1, usuario.getToken());
+            stat.setBoolean(2, usuario.getStatus());
             
             stat.execute();
             stat.close();
@@ -41,22 +40,20 @@ public class UsuarioDao implements UsuarioDaoIT {
     }
     
     @Override
-    public Usuario buscarUsuario(String email, String token){
+    public Usuario buscarUsuario(String token){
         Usuario usuario = new Usuario();
-        String sql = "select * from usuario where email ilike ? and token = ?";
+        String sql = "select * from usuario where token = ?";
         
         try {
             PreparedStatement stat = this.conn.prepareStatement(sql);
-            stat.setString(1, email);
-            stat.setString(2, token);
+            stat.setString(1, token);
             
             ResultSet rs = stat.executeQuery();
             
             if(rs.next() == true){
                 usuario.setId(rs.getInt(1));
-                usuario.setEmail(rs.getString(2));
-                usuario.setToken(rs.getString(3));
-                usuario.setStatus(rs.getBoolean(4));
+                usuario.setToken(rs.getString(2));
+                usuario.setStatus(rs.getBoolean(3));
             }else{
                 usuario = null;
             }
